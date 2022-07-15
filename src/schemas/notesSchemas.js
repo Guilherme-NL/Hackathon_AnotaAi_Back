4952a5b6656611d18acecbbe7688c55795d9c0c3
barbararech <rech.brb@gmail.com>
2022-07-15 19:34:26 -0300

    feat: add notes controllers

--- src/schemas/notesSchemas.js
+++ src/schemas/notesSchemas.js
@@ -1,5 +1,9 @@
 import joi from "joi";
 
 export const addNotesSchema = joi.object({
+  title: joi.string().min(5).required(),
   description: joi.string().min(20).required(),
+  date: joi.required(),
 });
+
+  
