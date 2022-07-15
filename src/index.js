f70dc9c6c3cb2517764c4972a89cb275ab0d7069
barbararech <rech.brb@gmail.com>
2022-07-15 19:04:56 -0300

    feat: add database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
+import notesRouter from "./routes/notesRouter.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const server = express();
-
 server.use(cors());
-server.use(express.json());
+server.use(json());
 
+server.use(notesRouter);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log('Server Online'));
+
+server.listen(PORT, () => console.log("Server On!"));
